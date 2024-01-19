e53e67152ce278a33045c48ad1a6ed084ec80d05
LudoPilot <e.berardfr@gmail.com>
2024-01-19 12:54:44 +0100

    Modification de environment.ts pour le déploiement sur Render.com

--- front-end/src/environments/environment.ts
+++ front-end/src/environments/environment.ts
@@ -1,8 +1,17 @@
+// export const environment = {
+// 	production: true,
+// 	backendRegisterClient: 'http://localhost:8000/api/register',
+// 	backendLoginClient: 'http://localhost:8000/api/utilisateur/login',
+// 	backendCatalogue: 'http://localhost:8000/api/catalogue',
+// 	backendCatalogueSearch: 'http://localhost:8000/api/catalogue/{filtre}',
+// 	backendLogoutClient: 'http://localhost:8000/api/utilisateur/logout'
+// };
+
 export const environment = {
 	production: true,
-	backendRegisterClient: 'http://localhost:8000/api/register',
-	backendLoginClient: 'http://localhost:8000/api/utilisateur/login',
-	backendCatalogue: 'http://localhost:8000/api/catalogue',
-	backendCatalogueSearch: 'http://localhost:8000/api/catalogue/{filtre}',
-	backendLogoutClient: 'http://localhost:8000/api/utilisateur/logout'
+	backendRegisterClient: 'https://projet-berard-ludovic.onrender.com/api/register',
+	backendLoginClient: 'https://projet-berard-ludovic.onrender.com/api/utilisateur/login',
+	backendCatalogue: 'https://projet-berard-ludovic.onrender.com/api/catalogue',
+	backendCatalogueSearch: 'https://projet-berard-ludovic.onrender.com/api/catalogue/{filtre}',
+	backendLogoutClient: 'https://projet-berard-ludovic.onrender.com/api/utilisateur/logout'
 };
