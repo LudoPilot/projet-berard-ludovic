46f1c157120761a23c030c07a80b79e9547d8031
LudoPilot <e.berardfr@gmail.com>
2024-01-24 10:51:24 +0100

    Redéploiement

--- front-end/src/environments/environment.ts
+++ front-end/src/environments/environment.ts
@@ -1,12 +1,3 @@
-// export const environment = {
-// 	production: true,
-// 	backendRegisterClient: 'http://localhost:8000/api/register',
-// 	backendLoginClient: 'http://localhost:8000/api/utilisateur/login',
-// 	backendCatalogue: 'http://localhost:8000/api/catalogue',
-// 	backendCatalogueSearch: 'http://localhost:8000/api/catalogue/{filtre}',
-// 	backendLogoutClient: 'http://localhost:8000/api/utilisateur/logout'
-// };
-
 export const environment = {
 	production: true,
 	backendRegisterClient: 'https://projet-berard-ludovic.onrender.com/api/register',
